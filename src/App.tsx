85e3817e28d0a47e63c77a016c3614305fefa247
Hazz-i <wahidh697@gmail.com>
2025-07-07 17:50:24 +0700

    first project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<>
+			<div>
+				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+				<h1>Welcome to the React App</h1>
+				<p>This is a simple React application.</p>
+				<p>Feel free to explore and modify the code!</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
